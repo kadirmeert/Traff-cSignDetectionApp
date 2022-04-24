13de277860cc4f5e65e7c35118e68bd35d3a42e2
kadirmeert <83037722+kadirmeert@users.noreply.github.com>
2022-04-25 00:25:27 +0300

    Initial Commit

---
+++ TraffıcSignDetectionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraffıcSignDetectionApp
+//
+//  Created by Kadır  Yıldız on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
